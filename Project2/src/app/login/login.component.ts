0a6d3c955b43dacb4df2e87a4ab11d55c25dab4a
TyreeG <graham.l.tyree@gmail.com>
2019-06-17 11:49:53 -0400

    unfinish login

--- Project2/src/app/login/login.component.ts
+++ Project2/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from '../services/current-user.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ export class LoginComponent implements OnInit {
   email = '';
   password = '';
 
-  constructor() { }
+  constructor(private login: CurrentUserService) { }
 
   ngOnInit() {
   }
