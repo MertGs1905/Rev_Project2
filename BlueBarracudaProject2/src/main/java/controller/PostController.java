d6b1c3adf2274f4fbcc25fe82bff2f129362cda7
TyreeG <graham.l.tyree@gmail.com>
2019-06-24 15:26:21 -0400

    added the rest of the controller

---
+++ BlueBarracudaProject2/src/main/java/controller/PostController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import model.Comment;
+import model.Post;
+import repository.PostRepo;
+
+@CrossOrigin(origins="hhtp://localhost:4200")
+@Controller
+@RequestMapping(value="/Project2")
+public class PostController {
+	
+	private PostRepo postRepo;
+	
+	public PostController() {
+		
+	}
+	
+	@Autowired
+	public PostController(PostRepo postRepo) {
+		
+		this.postRepo = postRepo;	
+	}
+	
+	@GetMapping(value="/getAllPost.app")
+	public @ResponseBody List<Post> getAllPost(){
+		return postRepo.SelectAll();
+		
+	}
+	
+	@PostMapping(value="/getCommentById.app")
+	public @ResponseBody Post getPostById(@RequestParam("id") int num) {
+		return postRepo.selectByInt(num);
+	}
+	
+	@PostMapping(value="{num}/getCommentByUri.app")
+	public @ResponseBody Post getPostByUri(@PathVariable("num") int num) {
+		return postRepo.selectByInt(num);
+	}
+	
+}
