a24a8910a3641d6cc68e4c31d87959bcee2f11ba
JacobShanklin <Jacob.Shanklin.2@gmail.com>
2019-06-23 21:37:04 -0400

    merge

--- BlueBarracudaProject2/src/main/java/model/Comment.java
+++ BlueBarracudaProject2/src/main/java/model/Comment.java
@@ -1,6 +1,21 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="comments")
 public class Comment {
+	@Id
+	@Column(name="food_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int commentId;
+	@OneToMany
 	User user;
 	String comment;
 	Ratings rating;
