926fa2c60308ae168fc8609f61b2dd74ad62b78c
MichaelGPerkins <michael.guy.perkins@gmail.com>
2019-06-25 00:21:20 -0400

    rebuilt controller classes on working tomcat server

---
+++ BlueBarracuda/src/main/java/com/bluebarracuda/controller/RatingController.java
@@ -0,0 +1,54 @@
+package com.bluebarracuda.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import model.Ratings;
+import repository.RatingRepo;
+
+
+
+@CrossOrigin(origins="http://localhost:4200")
+@Controller
+@RequestMapping(value="/Project2")
+public class RatingController {
+	
+
+	private RatingRepo ratingRepo;
+	
+	public RatingController() {
+		
+	}
+	
+	
+	@Autowired
+	public RatingController(RatingRepo ratingRepo) {
+		this.ratingRepo = ratingRepo;
+	}
+	
+	@GetMapping(value="/getAllRating.app")
+	public @ResponseBody List<Ratings> getAllRating(){
+		return ratingRepo.selectAll();
+	}
+	
+
+	@PostMapping(value="/getRatingById.app")
+	public @ResponseBody Ratings getRatingById(@RequestParam("id") int num) {
+		return ratingRepo.selectById(num);
+	}
+	
+	@PostMapping(value="{num}/getRatingByUri.app")
+	public @ResponseBody Ratings getRatingByUri(@PathVariable("num") int num) {
+		return ratingRepo.selectById(num);
+	}
+
+}
---
+++ BlueBarracuda/src/main/java/com/bluebarracuda/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.bluebarracuda.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@CrossOrigin(origins="http://localhost:4200")
+@Controller
+@RequestMapping(value="/Project2")
+public class UserController {
+	
+
+	private UserRepo userRepo;
+	
+	public UserController() {
+	}
+
+	@Autowired
+	public UserController(UserRepo userRepo) {
+		super();
+		this.userRepo = userRepo;
+	}
+	
+	@GetMapping(value="/getAllUser.app")
+	public @ResponseBody List<User> getAllUser(){
+		return userRepo.selectAll();
+	}
+	
+	@PostMapping(value="/getUserById.app")
+	public @ResponseBody User getUserById(@RequestParam("id") int num) {
+		return userRepo.selectById(num);
+	}
+	
+	@PostMapping(value="{num}/getUserByUri.app")
+	public @ResponseBody User getUserByUri(@PathVariable("num") int num) {
+		return userRepo.selectById(num);
+	}
+}
