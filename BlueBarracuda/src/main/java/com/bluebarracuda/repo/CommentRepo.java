bd4042bcb7b611f2bca9b14f4301a9fc7026263a
Arnold Sinko <arnoldcsinko@gmail.com>
2019-06-25 00:25:42 -0400

    move repos to new directory

---
+++ BlueBarracuda/src/main/java/com/bluebarracuda/repo/CommentRepo.java
@@ -0,0 +1,59 @@
+package com.bluebarracuda.repo;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bluebarracuda.model.Comment;
+
+@Repository("userRepo")
+@Transactional
+public class CommentRepo {
+		
+		static {
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private SessionFactory sesFact;
+
+		public CommentRepo() {
+		}
+		
+
+		@Autowired
+		public CommentRepo(SessionFactory sesFact) {
+			super();
+			this.sesFact = sesFact;
+		}
+		
+		public void insert(Comment com) {
+			
+			sesFact.getCurrentSession().save(com);
+		}
+		
+		public void update(Comment com) {
+			sesFact.getCurrentSession().update(com);
+		}
+		
+		public void delete(Comment com) {
+			sesFact.getCurrentSession().delete(com);;
+		}
+		
+		public Comment selectById(int id) {
+			return sesFact.getCurrentSession().get(Comment.class, id);
+		}
+		
+		public List<Comment> selectAll(){
+			return sesFact.getCurrentSession().createQuery("from Comment", Comment.class).list();
+		}
+		
+
+
+}
---
+++ BlueBarracuda/src/main/java/com/bluebarracuda/repo/UserRepo.java
@@ -0,0 +1,59 @@
+package com.bluebarracuda.repo;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bluebarracuda.model.User;
+
+
+
+@Repository("userRepo")
+@Transactional
+public class UserRepo {
+	
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private SessionFactory sesFact;
+
+	public UserRepo() {
+	}
+
+	@Autowired
+	public UserRepo(SessionFactory sesFact) {
+		super();
+		this.sesFact = sesFact;
+	}
+	
+	public void insert(User user) {
+		
+		sesFact.getCurrentSession().save(user);
+	}
+	
+	public void update(User user) {
+		sesFact.getCurrentSession().update(user);
+	}
+	
+	public void delete(User user) {
+		sesFact.getCurrentSession().delete(user);;
+	}
+	
+	public User selectById(int id) {
+		return sesFact.getCurrentSession().get(User.class, id);
+	}
+	
+	public List<User> selectAll(){
+		return sesFact.getCurrentSession().createQuery("from User", User.class).list();
+	}
+	
+
+}
