cad762b8f4448f73c328c3302bf9e056d96b8fe3
Arnold Sinko <arnoldcsinko@gmail.com>
2019-06-26 15:57:41 -0400

    fixed models

--- BlueBarracuda/src/main/java/com/bluebarracuda/controller/SessionController.java
+++ BlueBarracuda/src/main/java/com/bluebarracuda/controller/SessionController.java
@@ -1,7 +1,7 @@
 package com.bluebarracuda.controller;
 
 
-
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
