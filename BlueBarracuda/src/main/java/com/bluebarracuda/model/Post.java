403cd96a8de85d13c9a3b49dd71456f1bd7f7953
Arnold Sinko <arnoldcsinko@gmail.com>
2019-06-26 01:27:12 -0400

    added method to add newPost, fixed Post model and postRep

--- BlueBarracuda/src/main/java/com/bluebarracuda/model/Post.java
+++ BlueBarracuda/src/main/java/com/bluebarracuda/model/Post.java
@@ -1,78 +1,79 @@
 package com.bluebarracuda.model;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Post")
+@Table(name = "Post")
 public class Post {
-<<<<<<< HEAD
-	
+
 	@Id
-	@Column(name="food_id")
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	int id;
+	@Column(name = "post_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int postId;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "User_FK")
+	private User user;
 	
-	@OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(unique = true)
-	User user;
-		
-	String post;
-	int likes;
-	int dislikes;
+	@Column(name="post_text", nullable=false)	
+	private String postText;
 	
-	public Post() {		
+	@Column(name="likes")
+	private int likes;
+	
+	@Column(name="dislikes")
+	private int dislikes;
+
+	public Post() {
 	}
-	
-	public Post(User user, String post, int likes, int dislikes) {
-=======
-	User user;
-	String post;
-	Ratings rating;
-	public Post(User user, String post, Ratings rating) {
->>>>>>> 41253aa69d9f70c1e15c3ab46eb7573790bf6ffc
-		super();
+
+	public Post(User user, String postText, int likes, int dislikes) {
 		this.user = user;
-		this.post = post;
+		this.postText = postText;
 		this.likes = likes;
 		this.dislikes = dislikes;
 	}
+
 	@Override
 	public String toString() {
-<<<<<<< HEAD
-		return "Post [user=" 
-				+ user + ", post=" + post + ", likes=" + likes + ", dislikes " + dislikes +"]";
-=======
-		return "Post [user=" + user + ", post=" + post + ", rating=" + rating + "]";
+		return "Post [user=" + user 
+				+ ", post=" + postText 
+				+ ", likes=" + likes 
+				+ ", dislikes " + dislikes + "]";
 	}
+
 	public User getUser() {
 		return this.user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
->>>>>>> 41253aa69d9f70c1e15c3ab46eb7573790bf6ffc
+
 	}
-		
-	public String getPost() {
-		return post;
+
+	public String getPostText() {
+		return postText;
 	}
-	public void setPost(String post) {
-		this.post = post;
+
+	public void setPostText(String post) {
+		this.postText = post;
 	}
+
 	public int getLikes() {
 		return likes;
 	}
+
 	public void setLikes(int likes) {
 		this.likes = likes;
 	}
-	
-	
-	
+
 }
