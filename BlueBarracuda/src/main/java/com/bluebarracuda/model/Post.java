485842fe048d03894ff13519d1a4525b6639e210
Arnold Sinko <arnoldcsinko@gmail.com>
2019-06-26 00:03:17 -0400

    edited Post repo

--- BlueBarracuda/src/main/java/com/bluebarracuda/model/Post.java
+++ BlueBarracuda/src/main/java/com/bluebarracuda/model/Post.java
@@ -1,37 +1,61 @@
 package com.bluebarracuda.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Post")
 public class Post {
-	String user;
+	
+	@Id
+	@Column(name="food_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int id;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(unique = true)
+	User user;
+		
 	String post;
-	Ratings rating;
-	public Post(String user, String post, Ratings rating) {
+	int likes;
+	int dislikes;
+	
+	public Post() {		
+	}
+	
+	public Post(User user, String post, int likes, int dislikes) {
 		super();
 		this.user = user;
 		this.post = post;
-		this.rating = rating;
+		this.likes = likes;
+		this.dislikes = dislikes;
 	}
 	@Override
 	public String toString() {
-		return "Post [user=" + user + ", post=" + post + ", rating=" + rating + "]";
+		return "Post [user=" 
+				+ user + ", post=" + post + ", likes=" + likes + ", dislikes " + dislikes +"]";
 	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
+		
 	public String getPost() {
 		return post;
 	}
 	public void setPost(String post) {
 		this.post = post;
 	}
-	public Ratings getRating() {
-		return rating;
+	public int getLikes() {
+		return likes;
 	}
-	public void setRating(Ratings rating) {
-		this.rating = rating;
+	public void setLikes(int likes) {
+		this.likes = likes;
 	}
+	
+	
 	
 }
