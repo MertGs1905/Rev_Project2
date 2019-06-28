f6c924ff957ecd3faed0f32ad551ef9bf6d32fbc
MertGs <tunakapak@gmail.com>
2019-06-27 22:53:53 -0400

    vert

---
+++ BlueBarracuda/src/main/java/com/bluebarracuda/test/TestUsers.java
@@ -0,0 +1,63 @@
+package com.bluebarracuda.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import com.amazonaws.services.identitymanagement.model.User;
+import com.bluebarracuda.repo.UserRepo;
+
+import antlr.collections.List;
+
+public class TestUsers {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		UserRepo test1 = new UserRepo();
+		List usersAll = (List) test1.selectAll();
+	usersAll.equals(null);
+		assertFalse(usersAll.equals(null));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+	}
+
+}
